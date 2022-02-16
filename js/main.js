cf87bd741cef5f997461fa4ef1f9254f62f6ef58
kbo580 <kobayuta1025@gmail.com>
2022-02-16 16:42:50 +0900

    menuの制作

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
 $(function(){
 
+// バーガーアイコンをクリックでメニューを表示
+  $('#screen').hide();
+
   $('#show').on('click', function(){
     $(this).toggleClass('active');
     $('#nav').toggleClass('active');
+    $('.bar').toggleClass('colorChange');
+    $('#screen').fadeToggle();
     $('body').toggleClass('active');
   });
   
-  $('.nav-menu li a').on('click', function () {
+  $('.sp-nav-list a').on('click', function () {
       $('#show').removeClass('active');
       $('#nav').removeClass('active');
+      $('.bar').removeClass('colorChange');
+      $('#screen').fadeOut();
       $('body').removeClass('active');
   });
+
+  $('#screen').on('click', function(){
+    $('.sp-nav-list a').click();
+  });
+
+  //pc用カーソル
+  $('.pc-nav .nav-list a').hover(function(){
+    $(this).prev().css('opacity', '1');
+  
+  }, function(){
+    $(this).prev().css('opacity', '0');
+  });
+
+
+ 
 
 
 
