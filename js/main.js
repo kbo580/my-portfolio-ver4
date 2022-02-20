36cff9f03a7a915c7d8a93d5238f0b75731dd2f1
kbo580 <kobayuta1025@gmail.com>
2022-02-20 17:14:14 +0900

    スライダーの作成

--- js/main.js
+++ js/main.js
@@ -31,8 +31,34 @@ $(function(){
     $(this).prev().css('opacity', '0');
   });
 
+  // worksのitemにカーソルが乗った時の動き
+  $('.works__item-image-wrapper').hover(function(){
+    $(this).find('img').css('transform','scale(1.1, 1.1)');
+  },function(){
+    $(this).find('img').css('transform','scale(1, 1)');
+  });
+
 
  
+	$('.slider').slick({
+		// fade:true,//切り替えをフェードで行う。初期値はfalse。
+		// autoplay: true,//自動的に動き出すか。初期値はfalse。
+		// autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+		// speed:1000,//スライドの動きのスピード。初期値は300。
+		// infinite: true,//スライドをループさせるかどうか。初期値はtrue
+		arrows: true,
+		prevArrow: '<div class="prev-arrow"></div>',
+		nextArrow: '<div class="next-arrow"></div>',
+		dots: true,
+    //     pauseOnFocus: false,//フォーカスで一時停止を無効
+    //     pauseOnHover: false,//マウスホバーで一時停止を無効
+    //     pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    // $('.slider').slick('slickPlay');
+});
 
 
 
