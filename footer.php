406078f79d0f6b976e9e0c8cd7009cbdb134f504
kbo580 <kobayuta1025@gmail.com>
2022-03-27 11:31:49 +0900

    slickの設定

--- footer.php
+++ footer.php
@@ -2,9 +2,6 @@
     <p class="copy">&copy;2022 Yuta Kobayashi</p>
   </footer>  
 
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
-
   <?php wp_footer(); ?>
 </body>
 
