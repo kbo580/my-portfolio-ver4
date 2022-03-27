95bed119e59356680f957a8859983a375c28b71e
kbo580 <kobayuta1025@gmail.com>
2022-03-27 11:31:32 +0900

    slickの設定

--- functions.php
+++ functions.php
@@ -1,14 +1,21 @@
 <?php
+// slickの読み込み
+function slick_slider() {
+  wp_enqueue_style('slick', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+  wp_enqueue_style('slick-theme', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css');
+  wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery') );
+}
+add_action('wp_enqueue_scripts', 'slick_slider');
 
 // css,jquery,fontaweasomeの読み込み
 function my_files() {
   wp_enqueue_style('style-name', get_template_directory_uri() . '/css/styles.css' , array(), '1.0.0',);
-  // wp_enqueue_style('icon', 'https://use.fontawesome.com/releases/v6.1.0/css/all.css');
+  wp_enqueue_style('icon', 'https://use.fontawesome.com/releases/v6.1.0/css/all.css');
   wp_enqueue_script('script-name', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0.0',);
-  // <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
-  //   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
 }
-
 add_action('wp_enqueue_scripts', 'my_files');
+
+
+
 
 
