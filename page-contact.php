e380d9da4d22f9cb9e8671ba3ebd9bbe33be8966
kbo580 <kobayuta1025@gmail.com>
2022-03-28 19:48:54 +0900

    お問い合わせページ完成

--- page-contact.php
+++ page-contact.php
@@ -14,57 +14,14 @@
           <p class="contact-page-text">お忙しい中ご覧いただきありがとうございます。<br>お仕事の話はもちろん、サイトの感想など何でもお聞かせください。<span class="click-open">※は必須となります</span></p>
 
           <div class="form-wrapper">
-
-            <div class="form-parts rpg-frame">
-              <div class="rpg-fram rpg-border">
-
-                <label for="ofice-name" class="must form-parts__label">会社名</label>
-                <input type="text" id="ofice-name" placeholder="例）山田"> 
-              </div>
-            </div>
-
-            <div class="form-parts rpg-frame">
-              <div class="rpg-border">
-                <label for="name" class="must form-parts__label">お名前</label>
-                <input type="text" id="name"  placeholder="例）山田 太郎"> 
-              </div>
-            </div>
-
-            <div class="form-parts rpg-frame">
-              <div class="rpg-border">
-                <label for="email" class="form-parts__label">メールアドレス</label>
-                <input type="text" id="email" placeholder="例）abc@example.com"> 
-              </div>
-            </div>
-            
-            <div class="form-parts rpg-frame">
-              <div class="rpg-border">
-                <label for="title" class="form-parts__label">件名</label>
-                <input type="text" id="title"  placeholder="例）件名"> 
-              </div>
-            </div>
-            
-            <div class="form-parts rpg-frame">
-              <div class="rpg-border">
-                <label for="textarea" class="must form-parts__label">お問い合わせ内容</label>
-                <textarea id="textarea" placeholder="ご自由にお書きください"></textarea> 
-              </div>
-            </div>
-
-            <div class="acceptance">
-              <p>スパムメール防止のため、上記の記入事項にお間違いがなければお手数ですがチェックを入れてください。</p>
-            </div>
-
-            <div class="btn-wrapper">
-              <input type="submit" value="送信" class="btn">
-            </div>
+            <?php the_content(); ?>
             
           </div>
           <!-- form-wrapperここまで -->
 
         </div>
         <!-- /inner -->
-        
+
         </section>
       <!-- /contact -->
   
