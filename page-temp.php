3d41bd143f3e3035c46b3cae702381f76965e3bc
kbo580 <kobayuta1025@gmail.com>
2022-03-28 11:01:59 +0900

    テンプレートの作成

---
+++ page-temp.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+  <div class="container">
+    <?php get_sidebar(); ?>
+    
+    <div class="main-contents">
+    <section class="works page-sec">
+        <h2 class="heading page-heading"><span class="jp-title">制作物一覧</span>WORKS</h2>
+        
+        <!-- <div class="breadcrumbs">
+          <div class="breadcrumb_inner inner">
+            <span><a href="/">ページ名</a></span> >
+            <span><a href="/">ページ名</a></span> >
+            <span>ページ名</span> 
+          </div>
+        </div> -->
+
+        <?php get_template_part('template-parts/breadcrumbs') ?>
+        <!-- /breadcrumbs -->
+        
+      <!-- /works -->
+  
+    </div>
+    <!-- /main-contents -->
+    
+  </div>
+  <!-- /container -->
+
+  <?php get_footer(); ?>
+  
