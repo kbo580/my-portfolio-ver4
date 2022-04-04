8153c34ec08d24a9a19a10e0e4484003977a9824
kbo580 <kobayuta1025@gmail.com>
2022-04-04 15:09:27 +0900

    topへ戻るボタンの追加

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <footer>
     <p class="copy">&copy;2022 Yuta Kobayashi</p>
+
+    <div id="page-top" class="page-top">
+      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/to-top.png" alt="topへ戻るボタン"></a>
+    </div>
+    <!-- /page-top -->
   </footer>  
 
   <?php wp_footer(); ?>
