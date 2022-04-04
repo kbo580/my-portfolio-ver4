476cf95231044943ea6d9772ee96f301b386df28
kbo580 <kobayuta1025@gmail.com>
2022-04-04 09:03:51 +0900

    タイピングを追加

--- js/main.js
+++ js/main.js
@@ -81,6 +81,50 @@ jQuery(function($){
     window.removeEventListener( 'touchmove' , movefun, { passive: false }   );
   });
 
+    //メインビジュアルのタイピング
+  function TextTypingAnime() {
+    $('.TextTyping').each(function () {
+      var elemPos = $(this).offset().top - 50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      var thisChild = "";
+      if (scroll >= elemPos - windowHeight) {
+        thisChild = $(this).children();
+        thisChild.each(function (i) {
+          var time = 100;
+          $(this).delay(time * i).fadeIn(time);
+        });
+      } else {
+        thisChild = $(this).children();
+        thisChild.each(function () {
+          $(this).stop(); 
+          $(this).css("display", "none");
+        });
+      }
+    });
+  }
+
+  $(window).on('load', function () {
+    var element = $(".TextTyping");
+    element.each(function () {
+      var text = $(this).html();
+      var textbox = "";
+      text.split('').forEach(function (t) {
+        if (t !== " ") {
+          textbox += '<span>' + t + '</span>';
+        } else {
+          textbox += t;
+        }
+      });
+      $(this).html(textbox);
+
+    });
+
+    $('.TextTyping span:nth-child(14)').append('<br>');
+
+    TextTypingAnime();
+  });
+
 
 
 
